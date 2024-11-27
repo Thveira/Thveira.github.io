f8abe9f9b80f0258c3804fbfbfb33f394b3dfab3
Thveira <thiago7v.silva@gmail.com>
2024-11-27 14:21:58 -0300

    Create scripts.js

---
+++ scripts.js
@@ -0,0 +1,3 @@
+document.getElementById('courseImage').addEventListener('click', function() {
+    alert('Você clicou na imagem do curso! Saiba mais sobre como transformar sua carreira com nosso curso de gestão de tráfego pago.');
+});
