8b31f1038134681ee6d061c2d9767ea8a5dcf5aa
Thveira <thiago7v.silva@gmail.com>
2024-12-04 13:56:14 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+// Garantir rolagem suave para o link da âncora
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
